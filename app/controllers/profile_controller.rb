a187b7b79dbc020522e14e44b8529ee481cab5b0
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-04-11 19:27:31 +0200

    Generate profile and towatch controllers

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,14 @@
+class ProfileController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:show]
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
---
+++ app/helpers/to_watch_helper.rb
@@ -0,0 +1,2 @@
+module ToWatchHelper
+end
