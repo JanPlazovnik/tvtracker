ba5de52f001e3b575918a58082a838d90b7c8b6d
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-04-11 19:27:47 +0200

    Add routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/", to: redirect(path: "/to-watch")
+  get "to-watch", to: "to_watch#show"
+  get "user/:id/profile", to: "profile#show"
 end
