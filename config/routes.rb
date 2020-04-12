982ac7aaa89b8d5f682b9bdd44906c6d807ef809
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-04-12 19:31:07 +0200

    add series resources to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :series, :only => [:index]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: redirect(path: "/to-watch")
-  get "to-watch", to: "to_watch#show"
+  get "to-watch", to: "series#index"
   get "user/:id/profile", to: "profile#show"
 end
