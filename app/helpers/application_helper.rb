e5efc9ece13a68f31843fc7840f0834399e431b6
Jan Plazovnik <jan.plazovnik@gmail.com>
2020-05-04 04:38:43 +0200

    Add helpers to make my life less miserable ♥

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,24 @@ module ApplicationHelper
   def profileInitials
     current_user.first_name[0,1].upcase + current_user.last_name[0,1].upcase
   end
+  def getWatchedText(episode)
+    return "WATCHED!" if current_user.episodes.include?(episode)
+    "WATCHED?"
+  end
+  def getWatchedClass(episode)
+    return "watched" if current_user.episodes.include?(episode)
+  end
+  def whipUpEpisodeMeta(episode)
+    str = "<span>"
+    str += "#{episode.title} &middot; " if episode.title
+    str += "Aired #{episode.date_aired.to_date }" if episode.date_aired
+    str += "</span>"
+    str += "<p>#{episode.summary}</p>"
+    str.html_safe
+  end
+  def getAddedClass(id)
+    return ["btn-add", "added"] if current_user.series.any? {|show| show.tvdb_id == id}
+    ["btn-add"]
+  end
+
 end
